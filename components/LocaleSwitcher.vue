4bdb4c287130b0b3eb185a6ffdbc8fb5780687cd
Gemma <hello@gemma.dev>
2021-05-13 16:21:55 +0100

    Nuxt internationalisation demo

---
+++ components/LocaleSwitcher.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <ul class="flex">
+      <li
+        v-for="locale in availableLocales"
+        :key="locale"
+        class="py-4 px-4 text-xl font-bold"
+      >
+        <nuxt-link :to="switchLocalePath(locale)">
+          {{ locale }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  computed: {
+    availableLocales () {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container flex flex-col items-center">
+    <div class="flex flex-col items-center">
+      <h1 class="mb-4 text-pink-700 font-bold">
+        {{ home.Title }}
+      </h1>
+      <div class="mb-4 text-green-700">
+        {{ home.Body }}
+      </div>
+      <img :src="`http://localhost:1337${home.Image.url}`" class="w-1/4 mb-6" alt="Featured Image">
+      <LocaleSwitcher />
+    </div>
+  </div>
+</template>
+
+<script>
+import LocaleSwitcher from '~/components/LocaleSwitcher.vue'
+export default {
+  components: {
+    LocaleSwitcher
+  },
+  async asyncData ({ $strapi, i18n }) {
+    const home = await $strapi.$home.find({ _locale: i18n.locale })
+    return {
+      home
+    }
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
